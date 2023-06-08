e239fe5f35840b915b2970c834fb393a7fe10e82
mahfuzurrahman4044 <mahfuzurrahman4044@gmail.com>
2023-06-08 21:59:51 +0600

    classes

--- index.js
+++ index.js
@@ -25,9 +25,17 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const classesCollection = client.db("summer-camp").collection("classes");
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        app.get("/classes", async (req, res) => {
+            const result = await classesCollection.find().toArray();
+            res.send(result);
+        })
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
@@ -43,3 +51,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Summer Camp is running on port ${port}`)
 })
+
